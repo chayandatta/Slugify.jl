48e16bef70f149f2f90737a15ce6b92559443694
Chayan Datta <chayan.datta1996@gmail.com>
2023-04-20 16:36:57 +0530

    basic feat

--- src/slugify.jl
+++ src/slugify.jl
@@ -1,5 +1,19 @@
-module slugify
+module Slugify
 
-# Write your package code here.
+export Slugify
+
+replacement = "_"
+
+function slugify(str::AbstractString) :: String
+    split_str = split(str)
+    res = join(split_str, replacement)
+    return res
+end
+
+function slugify(str::AbstractString, replacement::String) :: String
+    split_str = split(str)
+    res = join(split_str, replacement)
+    return res
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using slugify
+using Slugify: slugify
 using Test
 
 @testset "slugify.jl" begin
-    # Write your tests here.
+    @test slugify("Hello World") == "Hello_World"
+    @test slugify("Hello World", "_") == "Hello_World"
+    @test slugify("Hello World", "+") == "Hello+World"
+    @test slugify("Hello Darkness My Old Friend") == "Hello_Darkness_My_Old_Friend"
+    @test slugify("foo bar baz", "_") == "foo_bar_baz"
 end
