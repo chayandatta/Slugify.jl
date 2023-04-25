190ec4ab432ab023b275619cdcfe198c072e3ecd
Chayan Datta <chayan.datta1996@gmail.com>
2023-04-25 19:14:05 +0530

    added unicode support

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using Slugify
 using Test
 
-@testset "slugify.jl" begin
+@testset "Slugify.jl" begin
     @test slugify("Hello World") == "Hello-World"
     @test slugify("Hello World", "_") == "Hello_World"
     @test slugify("Hello World", "+") == "Hello+World"
     @test slugify("Hello Darkness My Old Friend") == "Hello-Darkness-My-Old-Friend"
     @test slugify("foo bar baz", "_") == "foo_bar_baz"
+    @test slugify("we ♥ Julia") == "we-love-Julia"
+    @test slugify("we ♥ Julia","_") == "we_love_Julia"
 end
